81a0b79ba6aef82e674c8a99717207dc5e843608
ns3336699 <33606942+ns3336699@users.noreply.github.com>
2019-04-18 18:28:27 +0530

    manage the print statement and spaces

--- oslrtf.java
+++ oslrtf.java
@@ -4,21 +4,16 @@ class osproject
 static int i,j,temp,temp1,temp2,temp_bust1,temp_bust2,temp_bust3;
 static int []bt={2,4,8};
 static int []id={2132,2102,2153};
-
 static int []arrival_time={0,0,0};
 public int m1()
 {
-
-
 if(bt[0] >bt[1] && bt[0] > bt[2])
 {
 }
-
 if(bt[1] >bt[0] && bt[1] > bt[2])
 {
 System.out.println("second student ahve greasest bust time");
 }
-
 if(bt[2] >bt[0] && bt[2] > bt[1])
 {
         System.out.println("\nAs Last Student have greatest bust time so it will run till its  bust time will became equal to any other students.\n");
@@ -28,9 +23,7 @@ for(int i=bt[2];i>=bt[2]-4;i--)
      
       {
 	bt[2]=8-i;
-       System.out.println("Student c has given  chance id no."+id[2]+" new bust time is="+bt[2]);
-         
-         
+       System.out.println("Student c has given  chance id no."+id[2]+" new bust time is="+bt[2]);        
       }
 }
 }
@@ -44,11 +37,9 @@ public int m2()
 	   System.out.println("first student has lowest id no. so give chance to id"+id[0]);
       }
      else if (id[1]<id[0] && id[1]<id[2])
-      {
-	  
+      { 
 	  for(int j=4; j>=3; j--)
 	   {
-		
 		if(j==3)
 		{
 			bt[1]=j;
@@ -74,9 +65,7 @@ public int m2()
 public int m3()
 {
 	 if (id[0]<id[1] && id[0]<id[2])
-	 {
-		 
-		 
+	 {	 
 	 }
 	 else if(id[1]<id[0] && id[1]<id[2])
 		 
@@ -88,14 +77,11 @@ public int m3()
 		 {
 		 bt[1]=j;
 		 System.out.println("Student b has given chance id no."+id[1]+" new bust time is="+bt[1]);
-		 }	 
-		 
+		 }	 	 
 	 }
 	 }
 	 else if (id[2]<id[0] && id[2]<id[1])
-	 {
-		
-		 
+	 {		 
 	 }
 	 return 1;
 }
@@ -284,10 +270,6 @@ temp1=id[1];
 
 temp2=id[2];
 
-
-
-
-
 osproject os=new osproject();
 os.m1();
 os.m2();
@@ -325,7 +307,6 @@ System.out.println("Student Name\t"+"Student ID\t"+"Arrival time"+"\t\tCompletio
 System.out.println("a\t\t"+temp+"\t\t"+arrival_time[0]+"\t\t\t "+completion_time_a +" \t\t\t"+temp_bust1+"\t\t\t"+turn_around_time_a+" \t\t\t        \t\t"+waiting_time_a+ " \t                       ");
 
 System.out.println("b\t\t"+temp1+"\t\t"+arrival_time[1]+"\t\t\t "+completion_time_b +" \t\t\t"+temp_bust2+"\t\t\t"+turn_around_time_b+"\t\t\t"+average_turn_around_time+"\t\t\t"+waiting_time_b+"\t\t    "+average_waiting_time);
-
 	
 System.out.println("c\t\t"+temp2+"\t\t"+arrival_time[2]+"\t\t\t "+completion_time_c +" \t\t\t"+temp_bust3+"\t\t\t"+turn_around_time_c+"\t\t\t         \t\t"+waiting_time_c+"\t                      ");
 }
