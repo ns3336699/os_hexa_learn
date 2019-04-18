d2fe5a70f7d20a8683e8af51febc2b488a85f9db
ns3336699 <33606942+ns3336699@users.noreply.github.com>
2019-04-18 18:12:34 +0530

    store the id in temp variable

--- os3.java
+++ os3.java
@@ -1,72 +1,146 @@
 
-class osproject
+public int m4()
 {
-static int i,j,temp,temp1,temp2,temp_bust1,temp_bust2,temp_bust3;
-static int []bt={2,4,8};
-static int []id={2132,2102,2153};
-
-static int []arrival_time={0,0,0};
-public int m1()
+	if(bt[0]> bt[1] && bt[1]>bt[0])
+	{
+		
+	}
+	else if(bt[1]> bt[2] && bt[1]>bt[0])
+	{
+	}
+	else if(bt[2]> bt[1] && bt[2]>bt[0])
+	{
+		 for(int j=bt[2];j>=bt[2]-1;j--)
+		 { 
+		 if(j==2)
+		 {
+		 bt[2]=j;
+         System.out.println("Student c has given chance id no."+id[2]+" new bust time is="+bt[2]);
+		 
+		 }
+	}
+		
+}
+return 1;
+}
+public int m5()
 {
 
-
+	if(id[0]<id[1] && id[0]<id[2])
+	{
+		for(int j=bt[2];j>=bt[2]-1;j--)
+		 { 
+		 if(j==1)
+		 {
+		 bt[0]=j;
+         System.out.println("Student a has given chance id no."+id[0]+" new bust time is="+bt[0]);
+		
+		 }
+		 }
+	}
+	else if(id[1]<id[2] && id[1]<id[0])
+	{
+		for(int j=bt[1];j>=bt[1]-1;j--)
+		 { 
+		 if(j==1)
+		 {
+		 bt[1]=j;
+         System.out.println("Student b has given chance id no."+id[1]+" new bust time is="+bt[1]);
+		
+	      }
+		 }
+	}
+	else if(id[2]<id[1] && id[2]<id[0])
+	{
+	}
+	return 1;
+}
+public int m6()
+{
+	if(id[0]< id[1] || id[0]<id[2])
+	{
+		for(int j=bt[0];j>=bt[0]-1;j--)
+		 { 
+		 if(j==1)
+		   {
+                      
+		 bt[0]=j;
+         System.out.println("Student a has given chance id no."+id[0]+" new bust time is="+bt[0]);
+                    }
+		 }
+       }	
+	else if(id[1]< id[0] && id[1]<id[2])
+	{
+	}
+	else if(id[2]< id[0] && id[2]<id[1])
+	{
+	}
+	return 1;
+    }
+public int m7()
+{
 if(bt[0] >bt[1] && bt[0] > bt[2])
 {
 }
-
-if(bt[1] >bt[0] && bt[1] > bt[2])
+else if(bt[1] >bt[0] && bt[1] > bt[2])
 {
-System.out.println("second student ahve greasest bust time");
 }
-
-if(bt[2] >bt[0] && bt[2] > bt[1])
+else if(bt[2] >bt[0] && bt[2] > bt[1])
 {
-        System.out.println("\nAs Last Student have greatest bust time so it will run till its  bust time will became equal to any other students.\n");
-for(int i=bt[2];i>=bt[2]-4;i--)
+        for(int j=bt[2];j>=bt[2]-1;j--)
+		 { 
+                   if(j==1) 
+		 {
+		 bt[2]=j;
+         System.out.println("Student c has given chance id no."+id[2]+" new bust time is="+bt[2]);
+           }
+		 }
+}
+return 1;
+}
+
+public int m8()
 {
- if(i==4)
-     
-      {
-	bt[2]=8-i;
-       System.out.println("Student c has given  chance id no."+id[2]+" new bust time is="+bt[2]);
-         
-         
-      }
+	if(id[0]<id[1] && id[0]<id[2])
+	{
+		 
+	}
+	else if(id[1]<id[2] && id[1]<id[0])
+	{
+		for(int j=bt[1];j>=bt[1]-1;j--)
+		 { 
+		 if(j==0)
+		 {
+		 bt[1]=j;
+         System.out.println("Student b has given chance id no."+id[1]+" new bust time is="+bt[1]);
+		
+	      }
+		 }
+	}
+	else if(id[2]<id[1] && id[2]<id[0])
+	{
+	}
+return 1;
 }
-}
-return 4;
-}
-	public int m2()
+public int m9()
 {
-     if (id[0]<id[1] && id[0]<id[2])
-  {
-	   System.out.println("first student has lowest id no. so give chance to id"+id[0]);
-      }
-     else if (id[1]<id[0] && id[1]<id[2])
-      {
-
-	  for(int j=4; j>=3; j--)
-	   {
-
-		if(j==3)
-		{
-			bt[1]=j;
-     System.out.println("student b has given chance  id no."+id[1]+" new bust time is="+bt[1]);
-			if(bt[2]>bt[0] ||bt[2]>bt[1])
-			{
-	            bt[2]=j;
-
-        System.out.println("stutent c has given chance id no."+id[2]+" new bust time is="+bt[2]);
-			}
-
-		}
-	   }
-
-      }
-      else if (id[2]<id[0] && id[2]<id[1])
-       {
-	System.out.println("third student has lowest id no. so give chance to id="+id[2]);
-
-       }
- return 1;
-}
+	if(id[0]< id[1] || id[0]<id[2])
+	{
+		for(int j=bt[0];j>=bt[0]-1;j--)
+		 { 
+		 if(j==0)
+		   {
+                     
+		 bt[0]=j;
+         System.out.println("Student a has given chance id no."+id[0]+" new bust time is="+bt[0]);
+                    }
+		 }
+       }	
+	else if(id[1]< id[0] && id[1]<id[2])
+	{
+	}
+	else if(id[2]< id[0] && id[2]<id[1])
+	{
+	}
+	return 1;
+    }
